49f8192d7e24fbd76d01bc7ed7e626bd2414fd79
Tanagends <tanagends@gmail.com>
2024-06-13 21:43:46 +0200

    model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,4 +1,4 @@
-#!/usr/in/env python3
+#!/usr/bin/env python3
 """ SQLAlchemy model named User for a database table named users"""
 
 from sqlalchemy import Column, String, Integer
@@ -13,7 +13,12 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
-    email - Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
+
+print(User.__tablename__)
+
+for column in User.__table__.columns:
+    print("{}: {}".format(column, column.type))
