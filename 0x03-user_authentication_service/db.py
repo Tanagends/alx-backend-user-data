af4435409e71b5a6484512d296cb7233f45e3a07
Tanagends <tanagends@gmail.com>
2024-06-14 00:40:03 +0200

    all

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
+"""
+DB module
+"""
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
-from typing import Dict
 
 from user import Base, User
 
@@ -17,7 +19,8 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db",
+                                     echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -32,38 +35,55 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """adds user to database"""
-        usr = User(email=email, hashed_password=hashed_password)
-        self._session.add(usr)
+        """
+        Create a User object and save it to the database
+        Args:
+            email (str): user's email address
+            hashed_password (str): password hashed by bcrypt's hashpw
+        Return:
+            Newly created User object
+        """
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
         self._session.commit()
-        return usr
+        return user
 
     def find_user_by(self, **kwargs) -> User:
-        """returns first row of keyword arguments"""
-        valid = [k in User.__table__.columns for k in kwargs.keys()]
-
-        if not all(valid) or not kwargs:
-            raise InvalidRequestError
-
-        try:
-            usr = self._session.query(User).filter_by(**kwargs).first()
-        except AttributeError:
-            raise InvalidRequestError
-
-        if usr is None:
-            raise NoResultFound
-        return usr
+        """
+        Return a user who has an attribute matching the attributes passed
+        as arguments
+        Args:
+            attributes (dict): a dictionary of attributes to match the user
+        Return:
+            matching user or raise error
+        """
+        all_users = self._session.query(User)
+        for k, v in kwargs.items():
+            if k not in User.__dict__:
+                raise InvalidRequestError
+            for usr in all_users:
+                if getattr(usr, k) == v:
+                    return usr
+        raise NoResultFound
 
     def update_user(self, user_id: int, **kwargs) -> None:
-        """Updates usr"""
+        """
+        Update a user's attributes
+        Args:
+            user_id (int): user's id
+            kwargs (dict): dict of key, value pairs representing the
+                           attributes to update and the values to update
+                           them with
+        Return:
+            No return value
+        """
         try:
             usr = self.find_user_by(id=user_id)
         except NoResultFound:
-            raise ValueError
-
-        for key, update in kwargs.items():
-            if key in User.__table__.columns:
-                setattr(usr, key, update)
+            raise ValueError()
+        for k, v in kwargs.items():
+            if hasattr(usr, k):
+                setattr(usr, k, v)
             else:
                 raise ValueError
         self._session.commit()
--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
-""" SQLAlchemy model named User for a database table named users"""
-
-from sqlalchemy import Column, String, Integer
+"""
+user.py module
+"""
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+)
 
 Base = declarative_base()
 
 
 class User(Base):
-    """Defines a users table columns"""
-
+    """
+    Definition of class User
+    """
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
