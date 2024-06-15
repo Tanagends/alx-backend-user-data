0e20b4e8e45d6df11667c194822894c1cbe8471f
Tanagends <tanagends@gmail.com>
2024-06-16 01:13:04 +0200

    all

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,7 +9,11 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Auth required"""
-        return False
+        if path is None or not excluded_paths:
+            return True
+        if not path.endswith('/'):
+            path += '/'
+        return path not in excluded_paths
 
     def authorization_header(self, request=None) -> str:
         """Sets auth request header"""
