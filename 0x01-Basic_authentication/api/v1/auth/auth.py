c825d822cd0bc2b9f2107524d00dafc509d42177
Tanagends <tanagends@gmail.com>
2024-06-16 01:23:34 +0200

    all

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -17,7 +17,9 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """Sets auth request header"""
-        return None
+        if request is None:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Curr user"""
