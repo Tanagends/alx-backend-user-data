05001bdee476b7f6ed7722103011e920ea362469
Tanagends <tanagends@gmail.com>
2024-06-18 00:21:55 +0200

    auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -14,12 +14,12 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
-AUTH_TYPE = getenv('AUTH_TYPE'):
+AUTH_TYPE = getenv('AUTH_TYPE')
 if AUTH_TYPE == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
 elif AUTH_TYPE == "basic_auth":
-    import BasicAuth from api.v1.auth.basic_auth
+    from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 
 
